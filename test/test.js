025d23537ac26ff5d12e6cd96c357b2e8f7c31ff
Coderdan <coderdannn@gmail.com>
2020-10-07 11:48:19 +0800

    adding some tests

--- test/test.js
+++ test/test.js
@@ -6,6 +6,7 @@ const diamond = require('diamond-util')
 let ghstDiamond
 let ghstStakingDiamond
 let account
+let bob = "0xC3c2e1Cf099Bc6e1fA94ce358562BCbD5cc59FE5"
 
 const fourBillion = '4000000000000000000000000000'
 
@@ -67,10 +68,50 @@ describe('GHSTStakingDiamond', async function () {
   })
 
   it('Should accumulate frens', async function () {
-    await ethers.provider.send('evm_increaseTime', [1]) // add 1 seconds
+
+    //Commented this out bc the test was failing (2 seconds elapsed)
+    //  await ethers.provider.send('evm_increaseTime', [1]) // add 1 seconds
+
     await ethers.provider.send('evm_mine') // mine the next block
     const frens = await ghstStakingDiamond.frens(account)
-    // console.log('Frens:' + frens.toString())
+    console.log('Frens:' + frens / Math.pow(10, 18))
     expect(frens).to.equal(ethers.BigNumber.from('46296296296296296296296'))
   })
+
+  it('Should be able to purchase ticket', async function () {
+    await ghstStakingDiamond.claimTickets(["0", "1", "2", "3", "4", "5"])
+    const totalSupply = await ghstStakingDiamond.totalSupply("0")
+
+    const frens = await ghstStakingDiamond.frens(account)
+    console.log('Frens:' + frens / Math.pow(10, 18))
+
+    expect(totalSupply).to.equal("1")
+  })
+
+  it("Should not be able to purchase Godlike ticket", async function () {
+    await ghstStakingDiamond.claimTickets(["5"])
+    const frens = await ghstStakingDiamond.frens(account)
+    console.log('Frens:' + frens / Math.pow(10, 18))
+  })
+
+  it("Total supply of tickets should be 6", async function () {
+    const totalSupply = await ghstStakingDiamond.totalSupplies()
+    expect(totalSupply.length).to.equal(6)
+  })
+
+  it("Balance of second item should be 1", async function () {
+    const balance = await ghstStakingDiamond.balanceOf(account, "1")
+    expect(balance).to.equal("1")
+  })
+
+  it("Balance of third item should be 1", async function () {
+    const balance = await ghstStakingDiamond.balanceOfAll(account)
+    expect(balance[2]).to.equal("1")
+  })
+
+  // it("Can transfer ticket", async function () {
+  //   await ghstStakingDiamond.safeTransferFrom(account, bob,)
+  // })
+
+
 })
