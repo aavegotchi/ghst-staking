f6a3f83d560ab330981900e7a7ec8813031269cb
Coderdan <dmathieuva@gmail.com>
2021-09-23 21:01:55 +0800

    work on verifying facets

--- scripts/verify.js
+++ scripts/verify.js
@@ -1,14 +1,28 @@
-const { run } = require('hardhat')
+const { run } = require("hardhat");
 
-async function verify (address, facet) {
-  // const address = '0xfa7a3bb12848A7856Dd2769Cd763310096c053F1'; // deployed address
-  // const facet = 'StakingFacet'; // name of facet
-  await run('verifyFacet', {
+async function verify() {
+  const address = "0xae92aBB889F328e8ADA8434F8a00c45F586a57fE"; // deployed address
+  const facet = "StakingFacet"; // name of facet
+  await run("verifyFacet", {
     apikey: process.env.POLYGON_API_KEY,
     contract: address,
-    facet
-  })
+    facet,
+  });
 }
 
-// verify('0xfa7a3bb12848A7856Dd2769Cd763310096c053F1', 'StakingFacet');
-exports.VerifyFacet = verify
+verify()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
+
+exports.VerifyFacet = verify;
+
+//Staking facet:
+//0xa253460d993418dFF9Db9552e984a1890a71737A
+// 0xc5fD77f43f9cc3cd58300979BbD9aad1051DB76d
+// 0xAf97fb914498A81C2BEeC444e40aE676f48bd4F8
+// 0x98C65794535b5bB556D989E1fc37b8798D14d98E
+// 0xD18832D705D0EAF7A44c7C5699910c4be21252Bd
+// 0xae92aBB889F328e8ADA8434F8a00c45F586a57fE
