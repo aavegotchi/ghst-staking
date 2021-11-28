a1ac9a2536f3f9b9a4f8ff95131099f6a30da620
Coderdan <dmathieuva@gmail.com>
2021-11-28 13:33:30 +0800

    deploy refund script

--- scripts/updates/refundFrens.ts
+++ scripts/updates/refundFrens.ts
@@ -1,17 +1,15 @@
-import { run, ethers, network } from "hardhat";
+import { PopulatedTransaction } from "@ethersproject/contracts";
+import { ethers, network } from "hardhat";
+
 import {
-  convertPoolsAndRatesToString,
-  UpdateRateTaskArgs,
-} from "../../tasks/updateRates";
-import { PoolObject } from "../../types";
-import {
+  gasPrice,
   impersonate,
   maticStakingAddress,
   stakingDiamondUpgrader,
 } from "../helperFunctions";
+import { sendToMultisig } from "../libraries/multisig/multisig";
 
 const addresses = [
-  "0x534d8Bb59EF657e32F03A07E1F08dBE2421702ED",
   "0x323AcB6f6A930980Ad214900665805F678b8f813",
   "0x677975399cbd8aa7BD17A4B87C04Ed07A85978d4",
   "0x9A8ab692a6D73242C74a727Ac7587aEda778B131",
@@ -48,11 +46,16 @@ const addresses = [
   "0x73e9c88D26343eca36bB1297CfD0f8D6F02Ff8ea",
   "0x1670B5cdd17D4cAA012A75B830265B69F63E0100",
   "0x32606A1b77A7914371087e9bCB5F102Cf4c54233",
+  "0xa4e0e27cA70537fd9DA91676A2385cc00b87Cc40",
+  "0x8B77d38a521896b2447136D451Bf4E3C05EeF0a5",
+  "0xaf061E0906a4f6F696134a9EBE19b54d4aE04C02",
+  "0x9c3bAcade6BBE81a6238110a28628512ABf4eC4a",
+  "0xc3f855fB87742885566F1C12f332ec54df35F6fd",
+  "0xECE32FC3907c43B8594DE0239645Ccc020ae2961",
 ];
 
 const amounts = [
-  "3200",
-  "7000",
+  "18000",
   "45000",
   "7200",
   "72500",
@@ -88,6 +91,12 @@ const amounts = [
   "2746",
   "86985",
   "15739",
+  "205000",
+  "4200",
+  "189100",
+  "7500",
+  "4720",
+  "650",
 ];
 
 async function refundFrens() {
@@ -96,29 +105,46 @@ async function refundFrens() {
     maticStakingAddress
   );
 
-  stakingFacet = await impersonate(
-    stakingDiamondUpgrader,
-    stakingFacet,
-    ethers,
-    network
-  );
+  if (network.name === "matic") {
+    const {
+      LedgerSigner,
+    } = require("../../../aavegotchi-contracts/node_modules/@ethersproject/hardware-wallets");
 
-  let frens = await stakingFacet.frens(addresses[0]);
-  console.log(
-    `epoch frens before adjusting ${amounts[0]}:`,
-    ethers.utils.formatEther(frens)
-  );
+    const signer = new LedgerSigner(ethers.provider);
 
-  await stakingFacet.adjustFrens(
-    addresses,
-    amounts.map((amt) => ethers.utils.parseEther(amt))
-  );
+    const tx: PopulatedTransaction =
+      await stakingFacet.populateTransaction.adjustFrens(
+        addresses,
+        amounts.map((amt) => ethers.utils.parseEther(amt)),
+        { gasLimit: 800000 }
+      );
+    await sendToMultisig(stakingDiamondUpgrader, signer, tx, ethers);
+  } else {
+    stakingFacet = await impersonate(
+      stakingDiamondUpgrader,
+      stakingFacet,
+      ethers,
+      network
+    );
 
-  frens = await stakingFacet.frens(addresses[0]);
-  console.log(
-    "new epoch frens after adjusting:",
-    ethers.utils.formatEther(frens)
-  );
+    let frens = await stakingFacet.frens(addresses[0]);
+    console.log(
+      `epoch frens before adjusting by ${amounts[0]}:`,
+      ethers.utils.formatEther(frens)
+    );
+
+    await stakingFacet.adjustFrens(
+      addresses,
+      amounts.map((amt) => ethers.utils.parseEther(amt)),
+      { gasPrice: gasPrice }
+    );
+
+    frens = await stakingFacet.frens(addresses[0]);
+    console.log(
+      "new epoch frens after adjusting:",
+      ethers.utils.formatEther(frens)
+    );
+  }
 }
 
 if (require.main === module) {
